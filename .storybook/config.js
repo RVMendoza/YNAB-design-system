6cd4d1b9f8307ad4ddd6cab99ac99517ef8c7eab
RVMendoza <RVxMendoza@gmail.com>
2019-08-06 21:21:29 -0400

    Import addDecorator so it works

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,5 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import React from 'react'
 import { ThemeProvider } from 'theme-ui'
 import Theme from './theme'
 
