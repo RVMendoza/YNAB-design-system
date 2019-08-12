b93ad11668deea48d51dbd7d5526fa26bafc76c5
RVMendoza <RVxMendoza@gmail.com>
2019-08-11 20:43:36 -0400

    Namespace component

--- components/Button/YNABButton.js
+++ components/Button/YNABButton.js
@@ -1,18 +1,21 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
 
-const Button = ({
+const YNABButton = ({
   variant = 'primary',
   ...props
 }) =>
   <div
     sx={{
       px: '10px',
-      fontSize: 4, // picks up value from `theme.fontSizes[4]`
+      // fontSize: 4, // picks up value from `theme.fontSizes[4]`
       variant: `buttons.${variant}`,
+      fontFamily: 'body',
+      fontWeight: `bold`
     }}>
     {props.children}
+    <h1>hello okay</h1>
   </div>
 
 
-export default Button
+export default YNABButton
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -5,18 +5,18 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
 import { Welcome } from '@storybook/react/demo';
-import { Button } from '../components/YNABComponents';
+import { YNABButton } from '../components/YNABComponents';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Button', module)
   .add('with text', () =>
-    <Button variant="primary" onClick={action('clicked')}>Hello Button</Button>
+    <YNABButton variant="primary" onClick={action('clicked')}>Hello Button</YNABButton>
   )
   .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
+    <YNABButton onClick={action('clicked')}>
       <span role="img" aria-label="so cool">
         😀 😎 👍 💯
       </span>
-    </Button>
+    </YNABButton>
   ));
